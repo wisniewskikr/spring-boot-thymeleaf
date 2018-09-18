94b9c38443e5b5cc2e9e2c4da771c0cfd04edf48
Wisniewski <wisniewk@emea.roche.com>
2018-09-18 09:26:41 +0200

    Add login attempt service

--- src/main/java/pl/kwi/springboot/providers/CustomAuthenticationProvider.java
+++ src/main/java/pl/kwi/springboot/providers/CustomAuthenticationProvider.java
@@ -1,5 +1,6 @@
 package pl.kwi.springboot.providers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,18 +9,29 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Component;
 
+import pl.kwi.springboot.services.LoginAttemptService;
+
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+	
+	@Autowired
+	private LoginAttemptService loginAttemptService;
     
 	@Override
     public Authentication authenticate(Authentication auth) throws AuthenticationException {
 		
         String username = auth.getName();
         String password = auth.getCredentials().toString();
+        
+        if (loginAttemptService.isBlocked(username)) {
+        	throw new RuntimeException("blocked");
+        }
  
         if ("user".equals(username) && "password".equals(password)) {
+        	loginAttemptService.loginSucceeded(username);
             return new UsernamePasswordAuthenticationToken(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
         } else {
+        	loginAttemptService.loginFailed(username);
             throw new BadCredentialsException("External system authentication failed");
         }
         
