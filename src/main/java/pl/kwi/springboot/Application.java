7ad45b9e0599f0435237d8d095395e18ad6ef806
Wisniewski <wisniewk@emea.roche.com>
2018-12-12 09:53:18 +0100

    Improve Application class

--- src/main/java/pl/kwi/springboot/Application.java
+++ src/main/java/pl/kwi/springboot/Application.java
@@ -2,16 +2,9 @@ package pl.kwi.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
