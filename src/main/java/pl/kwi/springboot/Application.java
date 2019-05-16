353e3dd190b7fd4cc90c8aeb21080c924d2b5964
Wisniewski <wisniewk@emea.roche.com>
2019-05-16 16:04:05 +0200

    Improve SpringBoot version

--- src/main/java/pl/kwi/springboot/Application.java
+++ src/main/java/pl/kwi/springboot/Application.java
@@ -2,19 +2,12 @@ package pl.kwi.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
